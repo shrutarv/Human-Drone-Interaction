383977504881e385886866f36f453116fa0c1682
Apurva-rajashekhar <apurva.rajashekhar@tu-dortmund.de>
2022-07-12 19:13:14 +0200

    working accelerometer only

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from imu.interface_IMU import interface_IMU
+from tensorflow import keras
+from scipy.signal import resample
+import numpy as np
+
 
 
 RE = interface_IMU('D3:FE:9A:01:90:57')
+model = keras.models.load_model('/home/violet/Master_Thesis/Human-Drone-Interaction/model_acc_only')
+
+gestures = {
+    '0':'swipe_left',
+    '1':'swipe_right',
+    '2':'swipe_up',
+    '3':'swipe_down',
+    '4':'H cw crl',
+    '5':'H ccw crl',
+    '6':'V cw crl',
+    '7':'V ccw crl',
+    
+}
 
 while True:
     try:
@@ -16,8 +33,13 @@ while True:
                         log_done = True
         
         data_acc = RE.get_imu_data()
-        print(data_acc)#'acc{}'.format(data_acc.shape())
         # Add your code here
+        data_acc_resampled = np.array([resample(data_acc, 50, axis=0)])
+        prediction = model.predict(data_acc_resampled)
+        gesture_idx = np.argmax(prediction)
+        print(gestures[str(gesture_idx)])
+        print('Confidence: ', np.max(prediction)*100,'%')
+
 
         
     except(KeyboardInterrupt):
